ca63405fbfcd8dd79beda528445e2db37aac8b55
lpl <mohamed.ali.saidina+github@gmail.com>
2018-06-13 12:47:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,40 @@
 from setuptools import setup, find_packages
 from boom import __version__
 
-install_requires = ['gevent', 'requests>=2.3.0']
-
 description = ''
-
-for file_ in ('README', 'CHANGES', 'CONTRIBUTORS'):
+for file_ in ('README', 'CHANGES', 'CONTRIBUTORS', 'LICENCE'):
     with open('%s.rst' % file_) as f:
         description += f.read() + '\n\n'
 
-
-classifiers = [
-    "Development Status :: 5 - Production/Stable",
-    "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
-]
-
-setup(name='boom',
-      version=__version__,
-      url='https://github.com/tarekziade/boom',
-      packages=find_packages(),
-      long_description=description,
-      description="Simple HTTP Load tester",
-      author="Tarek Ziade",
-      author_email="tarek@ziade.org",
-      include_package_data=True,
-      zip_safe=False,
-      classifiers=classifiers,
-      install_requires=install_requires,
-      test_suite='unittest2.collector',
-      entry_points="""
-      [console_scripts]
-      boom = boom.boom:main
-      """)
+# TODO: Change name to publish in PYPI.
+setup(
+    name='boom',
+    version=__version__,
+    url='https://github.com/lee-pai-long/boom',
+    packages=find_packages(),
+    long_description=description,
+    description="Simple HTTP Load tester",
+    author="Lee Pai Long",
+    author_email="mohamed.ali.saidina+github@gmail.com",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    install_requires=[
+        'gevent==1.1.2',
+        'requests>=2.3.0',
+        'PyYAML>=3.12'
+    ],
+    test_suite='unittest2.collector',
+    entry_points={
+        "console_scripts": [
+            "boom = boom.boom:main"
+        ]
+    }
+)
